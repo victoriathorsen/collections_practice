d8c468bf1da8f8c5c3aafa08367483d1dbf0a38d
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-14 01:03:25 -0800

    commit tree 82ff3ed6fabb42549f8485718c8ba436d0b21e71

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        a<=>b
+    end    
+end
 
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b<=>a
+    end 
+end
+
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.size<=>b.size
+    end
+end
+
+
+def swap_elements(array)
+    array[0], array[1], array [2] = array[0], array [2], array[1]
+end
+
+
+def reverse_array(array)
+    array.reverse
+end
+
+
+def kesha_maker(array)
+    array.collect do |letter|
+        letter[2] = "$"
+        letter
+    end
+end
+
+
+def find_a(array)
+    array.select do |word|
+        word.start_with? ('a')
+    end
+end
+
+
+def sum_array(array)
+    array.sum
+end
+
+
+def add_s(array)
+    array.collect.with_index do |word, i|
+        if i == 1
+            word
+        else
+            word + "s"
+        end
+    end
+end
+
+
+#array.each_with_index.collect do |word,i|
+    #if (0) || (2..-1)
